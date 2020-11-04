0727af652d1d3b7b14e751013fa40bf412a04c51
Talia <taliav26@gmail.com>
2020-11-04 21:16:08 +0000

    Added i18n base file

---
+++ i18n/base.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>view</name>
+    <message>
+        <location filename="../view.qml" line="13"/>
+        <source>PUPILA: Pupil Annotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="32"/>
+        <source>&amp;File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="33"/>
+        <source>&amp;Open...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="36"/>
+        <source>&amp;Show Annotation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="39"/>
+        <source>Save &amp;As...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="41"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="44"/>
+        <source>&amp;Fit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="46"/>
+        <source>Ellipse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="49"/>
+        <source>&amp;Circle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="54"/>
+        <source>&amp;Image</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="57"/>
+        <source>Equalize histogram</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="63"/>
+        <source>Thresholding</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="74"/>
+        <source>Contrast adjustment</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="76"/>
+        <source>Gamma correction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="87"/>
+        <source>Logarithmic correction</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="100"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="101"/>
+        <source>&amp;About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="177"/>
+        <source>Drag and drop an image here to open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="387"/>
+        <source>Selected points</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="392"/>
+        <source>Point </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="403"/>
+        <source>Parameters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="406"/>
+        <source>Center</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="408"/>
+        <source>Major axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="409"/>
+        <source>Minor axis</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="410"/>
+        <source>Rotation angle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="412"/>
+        <source>Radius</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="422"/>
+        <source>Threshold value</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="443"/>
+        <source>Gamma gain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="466"/>
+        <source>Logarithmic gain</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="492"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="506"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="516"/>
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="572"/>
+        <source>Open image</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
