b511fc0edfbe8abda56fe4b3941d53fce4137006
Talia <taliav26@gmail.com>
2020-11-04 22:55:08 +0000

    Added translation to French

---
+++ i18n/qml_fr.ts
@@ -0,0 +1,172 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr_FR" sourcelanguage="en_US">
+<context>
+    <name>view</name>
+    <message>
+        <location filename="../view.qml" line="13"/>
+        <source>PUPILA: Pupil Annotation</source>
+        <translation>PUPILA: Pupil Annotation</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="32"/>
+        <source>&amp;File</source>
+        <translation>&amp;Fichier</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="33"/>
+        <source>&amp;Open...</source>
+        <translation>Ouvert</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="36"/>
+        <source>&amp;Show Annotation</source>
+        <translation>Afficher l&apos;annotation</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="39"/>
+        <source>Save &amp;As...</source>
+        <translation>&amp;Enregistrer sous...</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="41"/>
+        <source>&amp;Quit</source>
+        <translation>Quitter</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="44"/>
+        <source>&amp;Fit</source>
+        <translation>Ajus&amp;ter</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="46"/>
+        <source>Ellipse</source>
+        <translation>Ellipse</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="49"/>
+        <source>&amp;Circle</source>
+        <translation>Cercle</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="54"/>
+        <source>&amp;Image</source>
+        <translation>&amp;Image</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="57"/>
+        <source>Equalize histogram</source>
+        <translation>Égaliser l&apos;histogramme</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="63"/>
+        <source>Thresholding</source>
+        <translation>Limitation</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="74"/>
+        <source>Contrast adjustment</source>
+        <translation>Réglage du contraste</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="76"/>
+        <source>Gamma correction</source>
+        <translation>Correction gamma</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="87"/>
+        <source>Logarithmic correction</source>
+        <translation>Correction logarithmique</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="100"/>
+        <source>&amp;Help</source>
+        <translation>Ai&amp;de</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="101"/>
+        <source>&amp;About</source>
+        <translation>À propos</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="177"/>
+        <source>Drag and drop an image here to open</source>
+        <translation>Faites glisser et déposez une image ici pour l&apos;ouvrir</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="387"/>
+        <source>Selected points</source>
+        <translation>Points sélectionnés</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="392"/>
+        <source>Point </source>
+        <translation>Point</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="403"/>
+        <source>Parameters</source>
+        <translation>Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="406"/>
+        <source>Center</source>
+        <translation>Centre</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="408"/>
+        <source>Major axis</source>
+        <translation>Axe majeur</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="409"/>
+        <source>Minor axis</source>
+        <translation>Axe mineur</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="410"/>
+        <source>Rotation angle</source>
+        <translation>Angle de rotation</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="412"/>
+        <source>Radius</source>
+        <translation>Radius</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="422"/>
+        <source>Threshold value</source>
+        <translation>Valeur de limitation</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="443"/>
+        <source>Gamma gain</source>
+        <translation>Gain gamma</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="466"/>
+        <source>Logarithmic gain</source>
+        <translation>Gain logarithmique</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="492"/>
+        <source>Save</source>
+        <translation>Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="506"/>
+        <source>Cancel</source>
+        <translation>Annuler</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="516"/>
+        <source>Next</source>
+        <translation>Suivant</translation>
+    </message>
+    <message>
+        <location filename="../view.qml" line="572"/>
+        <source>Open image</source>
+        <translation>Ouvrir l&apos;image</translation>
+    </message>
+</context>
+</TS>
